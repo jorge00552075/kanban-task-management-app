c3c6ae956fc13c6c0585d796b0c8aef3d65a5a2d
Jorge Sendejo <jorge00552075@gmail.com>
2023-07-04 11:10:59 -0500

    add board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/sidebar";
+import Board from "./components/board/board";
 import "./App.scss";
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
-      <Sidebar />
-      <main>board</main>
-    </div>
+      <div className="appLayout">
+        <Sidebar />
+        <Board />
+      </div>
+    </>
   );
 }
