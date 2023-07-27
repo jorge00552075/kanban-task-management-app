4ae1c6c52ec87cdf6c205d7bb2640e12ada06749
Jorge Sendejo <jorge00552075@gmail.com>
2023-07-27 02:13:40 -0500

    add empty board content

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -2,13 +2,27 @@ import Column from "../column/column";
 import styles from "./board.module.scss";
 
 export default function Board() {
+  const renderEmptyBoard = (
+    <div className={styles.center}>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          This board is empty. Create a new column to get started.
+        </p>
+        <button className={styles.button} type="button">
+          + Add New Column
+        </button>
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.board}>
-      <div className={styles.container}>
+      {renderEmptyBoard}
+      {/* <div className={styles.columnContainer}>
         <Column />
         <Column />
         <Column />
-      </div>
+      </div> */}
     </div>
   );
 }
